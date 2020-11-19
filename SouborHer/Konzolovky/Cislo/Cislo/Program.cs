398930eb8708db655c4a853d53c241abcab3d18f
Martin Dub <martindub62@gmail.com>
2020-11-20 00:51:30 +0100

    First console game added

---
+++ SouborHer/Konzolovky/Cislo/Cislo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cislo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Myslim si cislo.. Jake to je?");
+            Random rd = new Random();
+            int cislo = rd.Next(1, 20);
+            int vstup = Convert.ToInt32(Console.ReadLine());
+            while (vstup != cislo) {
+                if (vstup < cislo) {
+                    Console.WriteLine("Vetsi");
+                }
+                if (vstup > cislo) {
+                    Console.WriteLine("Mensi");
+                }
+                vstup = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("CG");
+        }
+    }
+}
